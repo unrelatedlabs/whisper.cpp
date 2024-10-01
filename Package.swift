ff92916f2ba9222fb638cba4a1a2e1fe65c5c73b
Peter Kuhar <peter@azumio.com>
2024-10-01 16:28:37 -0700

    enable coreml for swift

--- Package.swift
+++ Package.swift
@@ -29,14 +29,18 @@ let package = Package(
                "CMakeLists.txt",
                "Makefile"
             ],
-            sources: [
+           sources: [
                 "ggml/src/ggml.c",
                 "src/whisper.cpp",
                 "ggml/src/ggml-aarch64.c",
                 "ggml/src/ggml-alloc.c",
                 "ggml/src/ggml-backend.c",
                 "ggml/src/ggml-quants.c",
-                "ggml/src/ggml-metal.m"
+                "ggml/src/ggml-metal.m",
+                "src/coreml/whisper-encoder.h",
+                "src/coreml/whisper-encoder.mm",
+                "src/coreml/whisper-encoder-impl.h",
+                "src/coreml/whisper-encoder-impl.mm"
             ],
             resources: [.process("ggml-metal.metal")],
             publicHeadersPath: "spm-headers",
@@ -44,7 +48,8 @@ let package = Package(
                 .unsafeFlags(["-Wno-shorten-64-to-32", "-O3", "-DNDEBUG"]),
                 .define("GGML_USE_ACCELERATE"),
                 .unsafeFlags(["-fno-objc-arc"]),
-                .define("GGML_USE_METAL")
+                .define("GGML_USE_METAL"),
+                .define("WHISPER_USE_COREML")
                 // NOTE: NEW_LAPACK will required iOS version 16.4+
                 // We should consider add this in the future when we drop support for iOS 14
                 // (ref: ref: https://developer.apple.com/documentation/accelerate/1513264-cblas_sgemm?language=objc)
